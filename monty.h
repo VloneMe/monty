7dee30901b1d8c2408ecd2b154b643f154b1c2d7
VloneMe <francisntima@yahoo.com>
2023-06-22 09:53:10 -0700

    main header files

---
+++ monty.h
@@ -0,0 +1,54 @@
+#ifndef __MONTY_H__
+#define __MONTY_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+
+
+/*#### Data Stractures ####*/
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/*### Monty Prototypes ###*/
+
+int monty_main(int ac, char **av);
+
+void monty_free_stack(stack_t **stack);
+
+int monty_run(FILE *fd);
+char **tokening(char *line, char *delim);
+int empty_line(char *line, char *delims);
+int execute(char **token, stack_t **stack, unsigned int line_num);
+
+#endif /* __MONTY_H__ */
