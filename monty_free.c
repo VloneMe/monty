6b7a5a112d9bccc76fa855127c5f2d40a9d982d5
VloneMe <francisntima@yahoo.com>
2023-06-22 17:40:28 -0700

    free monty stack

---
+++ monty_free.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * free_stack - Free the memory allocated for the stack
+ *
+ * @stack: Pointer to the doubly linked list representation of a stack
+ * Return: void
+ */
+void free_stack(stack_t **stack)
+{
+	stack_t *current = *stack;
+	stack_t *tmp = NULL;
+
+	while (current != NULL)
+	{
+		tmp = current->next;
+		free(current);
+		current = tmp;
+	}
+
+	*stack = NULL;
+}
